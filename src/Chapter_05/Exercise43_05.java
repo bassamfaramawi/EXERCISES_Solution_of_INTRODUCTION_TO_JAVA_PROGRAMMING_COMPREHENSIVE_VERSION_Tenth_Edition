92d2e3d653f0761a072683cfb8ca0c5311e12439
Bassam Faramawi <tiodaronzi3@yahoo.com>
2018-10-11 09:23:31 -1100

    Exercises Solutions of Chapter 05

--- src/Chapter_05/Exercise43_05.java
+++ src/Chapter_05/Exercise43_05.java
@@ -1,7 +1,7 @@
 /*
- *5.43 (Math: combinations) Write a program that displays all possible combinations
-for picking two numbers from integers 1 to 7. Also display the total number of
-all combinations.
+ *5.43 (Math: combinations) Write a program that displays all possible 
+combinations for picking two numbers from integers 1 to 7. Also display the 
+total number of all combinations.
  */
 package Chapter_05;
 

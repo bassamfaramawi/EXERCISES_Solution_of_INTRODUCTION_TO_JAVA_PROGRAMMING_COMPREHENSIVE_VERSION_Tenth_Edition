2fbe3f03a771552ac1095b8a0d1833ff39f5d153
Bassam Faramawi <tiodaronzi3@yahoo.com>
2018-10-14 07:08:05 +0300

    Exercises solutions of Chapter 05

--- src/Chapter_05/Exercise20_05.java
+++ src/Chapter_05/Exercise20_05.java
@@ -16,7 +16,7 @@ public class Exercise20_05 {
         int count = 0; // Count the number of prime numbers
         int number = 2; // A number to be tested for primeness
         
-        System.out.println("The first 50 prime numbers are \n");
+        System.out.println("The prime numbers between 2 and 1000 are:");
         
         // Repeatedly find prime numbers
         while (number <= 1000) {
@@ -31,20 +31,20 @@ public class Exercise20_05 {
                 }
             }
               
-        // Display the prime number and increase the count
-        if (isPrime) {
-            count++; // Increase the count
-       
-            if (count % NUMBER_OF_PRIMES_PER_LINE == 0) {
-                // Display the number and advance to the new line
-                System.out.println(number);
-            }
-            else
-                System.out.print(number + " ");
-        }
-       
-        // Check if the next number is prime
-        number++;
+            // Display the prime number and increase the count
+			if (isPrime) {
+				count++; // Increase the count
+		   
+				if (count % NUMBER_OF_PRIMES_PER_LINE == 0) {
+					// Display the number and advance to the new line
+					System.out.println(number);
+				}
+				else
+					System.out.print(number + " ");
+			}
+		   
+			// Check if the next number is prime
+			number++;
         }
     }
     
